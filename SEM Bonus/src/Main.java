bf67cf214a09b1a175d632b059788821ef15ecc2
Marius Lämmlin <marius.laemmlin@gmail.com>
2017-01-28 19:31:47 +0100

    Änderung rückgängig

--- SEM Bonus/src/Main.java
+++ SEM Bonus/src/Main.java
@@ -7,8 +7,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Änderung!");
-
 		// displayPValsPositiveOnly();
 
 		solveLGS(createMatrix(), createBVector()); // Matrix und b-Vektor
