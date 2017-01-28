5810c5d0bc049e6cad3bf9c6ebc14dd24a5a33fb
svenkilian <kosven@web.de>
2017-01-28 19:31:19 +0100

    Nothing changed

--- SEM Bonus/src/Main.java
+++ SEM Bonus/src/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Änderung!");
-		
+		System.out.println("Ã„nderung!");
+
 		// displayPValsPositiveOnly();
 
 		solveLGS(createMatrix(), createBVector()); // Matrix und b-Vektor
